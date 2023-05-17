7cf3d1819a2cda0bd431d96ac5197273062c1552
Mariam2Waleed <Mariam2waleed@gmail.com>
2023-05-17 21:17:07 +0300

    app Drawer

--- lib/components/AppDrawer.dart
+++ lib/components/AppDrawer.dart
@@ -1,13 +1,5 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tutor/components/comp.dart';
-import 'package:tutor/pages/Classwork.dart';
-import 'package:tutor/pages/Inbox.dart';
-import 'package:tutor/pages/Home.dart';
-import 'package:tutor/pages/Notification.dart';
-import 'package:tutor/pages/Profile.dart';
-import 'package:tutor/pages/Schedule.dart';
 
 import '../service/store.dart';
 
@@ -20,48 +12,32 @@ class _AppDrawerState extends State<AppDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: ListView(children: <Widget>[
+        child: ListView(padding: EdgeInsets.zero, children: <Widget>[
       DrawerHeader(
-          child: Image.asset(currUser!.profileImage, fit: BoxFit.fitWidth)),
-      const ListTile(
-        title: Text("Item 1"),
-      ),
-      const ListTile(
-        title: Text("Item 2"),
-      )
+          child: Column(children: [
+        CircleAvatar(
+            radius: 50,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child:
+                    Image.asset(currUser!.profileImage, fit: BoxFit.fitWidth))),
+        // Image.asset(currUser!.profileImage, fit: BoxFit.fitWidth),
+        Text('Welcome ${currUser!.fname}', style: TextStyle(color: black))
+      ])),
+      Column(children: [
+        const ListTile(leading: Icon(Icons.message), title: Text("Item 1")),
+        const ListTile(
+            leading: Icon(Icons.account_circle), title: Text("Item 2")),
+        const ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
+        ListTile(
+            leading: const Icon(Icons.change_history),
+            title: const Text('Change history'),
+            onTap: () => {
+                  // change app state...
+                  Navigator.pop(context) // close the drawer
+                })
+      ])
     ]));
   }
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//  Drawer AppDrawer() {
-//     return Drawer(
-//           child: ListView(padding: EdgeInsets.zero, children: const <Widget>[
-//         DrawerHeader(
-//             decoration: BoxDecoration(color: Colors.amber),
-//             child: Text(' Welcome ',
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 24,
-//                 ))),
-//         ListTile(
-//           leading: Icon(Icons.message),
-//           title: Text('Messages'),
-//         ),
-//         ListTile(
-//           // leading: Icon(Icons.account_circle),
-//           title: Text('Profile'),
-//         ),
-//         ListTile(
-//           leading: Icon(Icons.settings),
-//           title: Text('Settings'),
-//         ),
-//         ListTile(
-//           leading: const Icon(Icons.change_history),
-//           title: const Text('Change history'),
-//           // onTap: () => {
-//           //   // change app state...
-//           //   Navigator.pop(context); // close the drawer
-//           // };
-//         )
-//       ]));
